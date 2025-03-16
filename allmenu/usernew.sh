2222494816bbdc5f412f0c91cb4f1637cd7af5cb
kanghory <132531297+kanghory@users.noreply.github.com>
2025-03-16 15:32:47 +0700

    Update usernew.sh

--- allmenu/usernew.sh
+++ allmenu/usernew.sh
@@ -1,75 +1,37 @@
-#!/bin/bash
+echo -e "\033[1;93m---------------------------------------------------\033[0m"
+echo -e " SSH Ovpn Account           "
+echo -e "\033[1;93m---------------------------------------------------\033[0m"
+read -p " Username : " Login
+read -p " Password : " Pass
+read -p " Limit IP : " iplimit
+read -p " Expired (Days) : " masaaktif
 
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[0;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export LIGHT='\033[0;37m'
-export NC='\033[0m'
-export ungu='\033[0;35m'
-
-
+# Limit IP
+if [[ $iplimit -gt 0 ]]; then
+    mkdir -p /etc/klmpk/limit/ssh/ip/
+    echo -e "$iplimit" > /etc/klmpk/limit/ssh/ip/$Login
+else
+    echo > /dev/null
+fi
 
 # Getting
-
 domain=$(cat /etc/xray/domain)
 sldomain=$(cat /root/nsdomain)
 cdndomain=$(cat /root/awscdndomain)
 slkey=$(cat /etc/slowdns/server.pub)
 clear
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m                 ⇱ CREATE ACCOUNT ⇲            \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "${CYAN}"
-read -p "Username : " Login
-read -p "Password : " Pass
-read -p "Expired (Days): " masaaktif
 
-IP=$(wget -qO- ipinfo.io/ip);
-ws="$(cat ~/log-install.txt | grep -w "Websocket TLS" | cut -d: -f2|sed 's/ //g')"
-ws2="$(cat ~/log-install.txt | grep -w "Websocket None TLS" | cut -d: -f2|sed 's/ //g')"
+IP=$(wget -qO- ipinfo.io/ip)
+ws="$(cat ~/log-install.txt | grep -w "Websocket TLS" | cut -d: -f2 | sed 's/ //g')"
+ws2="$(cat ~/log-install.txt | grep -w "Websocket None TLS" | cut -d: -f2 | sed 's/ //g')"
 
-ssl="$(cat ~/log-install.txt | grep -w "Stunnel5" | cut -d: -f2)"
-sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
-ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
-ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
-clear
-systemctl stop client-sldns
-systemctl stop server-sldns
-pkill sldns-server
-pkill sldns-client
-systemctl enable client-sldns
-systemctl enable server-sldns
-systemctl start client-sldns
-systemctl start server-sldns
-systemctl restart client-sldns
-systemctl restart server-sldns
-systemctl restart ws-tls
-systemctl restart ws-nontls
-systemctl restart ssh-ohp
-systemctl restart rc-local
-systemctl restart dropbear-ohp
-systemctl restart openvpn-ohp
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e "$Pass\n$Pass\n" | passwd $Login &> /dev/null
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
+
 clear
-echo -e ""
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m                 ⇱ INFORMASI AKUN SSH ⇲            \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
@@ -77,6 +39,7 @@ echo -e "${LIGHT}Username: $Login"
 echo -e "Password: $Pass"
 echo -e "Created: $hariini"
 echo -e "Expired: $expi"
+echo -e "Limit IP: $iplimit"
 echo -e "${LIGHT}=================HOST-SSH===================="
 echo -e "IP/Host: $IP"
 echo -e "Domain SSH: $domain"
@@ -94,20 +57,10 @@ echo -e "SSH Websocket HTTP: 80,8080"
 echo -e "SSH Websocket Direct: 8080"
 echo -e "OPEN VPN: 1194"
 echo -e "BadVPN UDPGW: 7100,7200,7300"
-echo -e "Proxy CloudFront: [OFF]"
 echo -e "Proxy Squid: [ON]"
 echo -e "OVPN TCP: http://$IP:81/tcp.ovpn"
 echo -e "OVPN UDP: http://$IP:81/udp.ovpn"
 echo -e "OVPN SSL: http://$IP:81/ssl.ovpn"
 echo -e "=================================================="
-echo -e "SNI/Server Spoof: isi dengan bug"
-echo -e "Payload Websocket SSL/TLS"
-echo -e "=================================================="
-echo -e "GET wss://bug.com/ HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "=================================================="
-echo -e "Payload Websocket HTTP"
-echo -e "=================================================="
-echo -e "GET / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "=================================================="
-echo -e "${CYAN}       Script By KANGHORY TUNNELING" 
+echo -e "${CYAN}       Script By kanghoryVPN" 
 echo -e "${LIGHT}=================================================="
