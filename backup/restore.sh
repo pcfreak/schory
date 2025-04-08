01199596dc3fba628be2f9743b7fc991ab441f7c
kanghory <132531297+kanghory@users.noreply.github.com>
2025-04-08 18:56:03 +0700

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -31,7 +31,7 @@ cp gshadow /etc/
 cp crontab /etc/
 cp chap-secrets /etc/ppp/
 cp passwd1 /etc/ipsec.d/passwd
-cp -rf klmpk /etc/
+cp -rf klmpk/. /etc/klmpk/
 cp -r crot /var/lib/
 cp -r sstp /home/
 cp -r xray /etc/
