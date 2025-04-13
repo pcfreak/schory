25a0f53822764b5f3b459360a400f28240caa272
kanghory <132531297+kanghory@users.noreply.github.com>
2025-04-13 14:06:10 +0700

    Update menun-ssh.sh

--- allmenu/menun-ssh.sh
+++ allmenu/menun-ssh.sh
@@ -92,42 +92,51 @@ function del() {
 }
 function autodel(){
 clear
-hariini=`date +%d-%m-%Y`
+hariini=$(date +%d-%m-%Y)
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[0;41;36m               AUTO DELETE                \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "Thank you for removing the EXPIRED USERS"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
-totalaccounts=`cat /tmp/expirelist.txt | wc -l`
-for((i=1; i<=$totalaccounts; i++ ))
-do
-tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
-username=`echo $tuserval | cut -f1 -d:`
-userexp=`echo $tuserval | cut -f2 -d:`
-userexpireinseconds=$(( $userexp * 86400 ))
-tglexp=`date -d @$userexpireinseconds`
-tgl=`echo $tglexp |awk -F" " '{print $3}'`
-while [ ${#tgl} -lt 2 ]
-do
-tgl="0"$tgl
+totalaccounts=$(cat /tmp/expirelist.txt | wc -l)
+
+for ((i=1; i<=$totalaccounts; i++ )); do
+    tuserval=$(head -n $i /tmp/expirelist.txt | tail -n 1)
+    username=$(echo $tuserval | cut -f1 -d:)
+    userexp=$(echo $tuserval | cut -f2 -d:)
+    userexpireinseconds=$(( userexp * 86400 ))
+    tglexp=$(date -d @$userexpireinseconds)
+    tgl=$(echo $tglexp | awk -F" " '{print $3}')
+    
+    while [[ ${#tgl} -lt 2 ]]; do tgl="0${tgl}"; done
+    while [[ ${#username} -lt 15 ]]; do username="$username "; done
+
+    bulantahun=$(echo $tglexp | awk -F" " '{print $2,$6}')
+    echo "echo \"Expired- User : $username Expire at : $tgl $bulantahun\"" >> /usr/local/bin/alluser
+
+    todaystime=$(date +%s)
+    if [[ $userexpireinseconds -ge $todaystime ]]; then
+        continue
+    else
+        echo "echo \"Expired- Username : $username are expired at: $tgl $bulantahun and removed : $hariini \"" >> /usr/local/bin/deleteduser
+        echo "Username $username that are expired at $tgl $bulantahun removed from the VPS $hariini"
+
+        # Hapus user dari sistem
+        userdel "$username"
+
+        # Hapus limit IP
+        limit_ip_file="/etc/klmpk/limit/ssh/ip/$username"
+        [[ -f "$limit_ip_file" ]] && rm -f "$limit_ip_file"
+
+        # Hapus limit kuota
+        kuota_dir="/etc/klmpk/limit/ssh/kuota"
+        limit_kuota_file="${kuota_dir}/${username}-limit"
+        used_kuota_file="${kuota_dir}/${username}-used"
+        [[ -f "$limit_kuota_file" || -f "$used_kuota_file" ]] && rm -f "$limit_kuota_file" "$used_kuota_file"
+    fi
 done
-while [ ${#username} -lt 15 ]
-do
-username=$username" "
-done
-bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
-echo "echo "Expired- User : $username Expire at : $tgl $bulantahun"" >> /usr/local/bin/alluser
-todaystime=`date +%s`
-if [ $userexpireinseconds -ge $todaystime ] ;
-then
-:
-else
-echo "echo "Expired- Username : $username are expired at: $tgl $bulantahun and removed : $hariini "" >> /usr/local/bin/deleteduser
-echo "Username $username that are expired at $tgl $bulantahun removed from the VPS $hariini"
-userdel $username
-fi
-done
+
 echo " "
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 read -n 1 -s -r -p "Press any key to back on menu"
