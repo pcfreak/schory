404459f7c137afad15f4094cf97b9b19552ff6dd
kanghory <132531297+kanghory@users.noreply.github.com>
2025-04-13 14:35:02 +0700

    Update menun-ssh.sh

--- allmenu/menun-ssh.sh
+++ allmenu/menun-ssh.sh
@@ -98,29 +98,33 @@ echo -e "\E[0;41;36m               AUTO DELETE                \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "Thank you for removing the EXPIRED USERS"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
 cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
 totalaccounts=$(cat /tmp/expirelist.txt | wc -l)
 
-for ((i=1; i<=$totalaccounts; i++ )); do
+for ((i=1; i<=totalaccounts; i++)); do
     tuserval=$(head -n $i /tmp/expirelist.txt | tail -n 1)
-    username=$(echo $tuserval | cut -f1 -d:)
-    userexp=$(echo $tuserval | cut -f2 -d:)
+    username=$(echo "$tuserval" | cut -f1 -d:)
+    userexp=$(echo "$tuserval" | cut -f2 -d:)
     userexpireinseconds=$(( userexp * 86400 ))
     tglexp=$(date -d @$userexpireinseconds)
     tgl=$(echo $tglexp | awk -F" " '{print $3}')
     
     while [[ ${#tgl} -lt 2 ]]; do tgl="0${tgl}"; done
-    while [[ ${#username} -lt 15 ]]; do username="$username "; done
+
+    # Username dengan padding hanya untuk ditampilkan
+    username_pad="$username"
+    while [[ ${#username_pad} -lt 15 ]]; do username_pad="$username_pad "; done
 
     bulantahun=$(echo $tglexp | awk -F" " '{print $2,$6}')
-    echo "echo \"Expired- User : $username Expire at : $tgl $bulantahun\"" >> /usr/local/bin/alluser
+    echo "echo \"Expired- User : $username_pad Expire at : $tgl $bulantahun\"" >> /usr/local/bin/alluser
 
     todaystime=$(date +%s)
     if [[ $userexpireinseconds -ge $todaystime ]]; then
         continue
     else
-        echo "echo \"Expired- Username : $username are expired at: $tgl $bulantahun and removed : $hariini \"" >> /usr/local/bin/deleteduser
-        echo "Username $username that are expired at $tgl $bulantahun removed from the VPS $hariini"
+        echo "echo \"Expired- Username : $username_pad are expired at: $tgl $bulantahun and removed : $hariini \"" >> /usr/local/bin/deleteduser
+        echo "Username $username_pad that are expired at $tgl $bulantahun removed from the VPS $hariini"
 
         # Hapus user dari sistem
         userdel "$username"
