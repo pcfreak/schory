5559c282e3bd87f920e5e520b27b9a5774eaa1a9
kanghory <132531297+kanghory@users.noreply.github.com>
2025-04-20 14:12:29 +0700

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -2,6 +2,7 @@
 echo -e cheking update
 sleep 2
 #hapus
+rm -f /usr/bin/update-script
 rm -f /usr/bin/menu
 rm -f /usr/bin/menun-ssh
 rm -f /usr/bin/menu_pw_host
@@ -10,6 +11,7 @@ rm -f /usr/bin/usernew
 rm -f /usr/bin/backup
 rm -f /usr/bin/restore
 #download
+wget -q -O /usr/bin/update-script "https://raw.githubusercontent.com/kanghory/schory/main/update-script.sh"
 wget -O /usr/bin/menu "https://raw.githubusercontent.com/kanghory/schory/main/allmenu/menu.sh"
 wget -O /usr/bin/menun-ssh "https://raw.githubusercontent.com/kanghory/schory/main/allmenu/menun-ssh.sh"
 wget -O /usr/bin/menu_pw_host "https://raw.githubusercontent.com/kanghory/schory/main/allmenu/menu_pw_host.sh"
@@ -18,6 +20,7 @@ wget -O /usr/bin/usernew "https://raw.githubusercontent.com/kanghory/schory/main
 wget -O /usr/bin/backup "https://raw.githubusercontent.com/kanghory/schory/main/backup/backup.sh"
 wget -O /usr/bin/restore "https://raw.githubusercontent.com/kanghory/schory/main/backup/restore.sh"
 #izin
+chmod +x /usr/bin/update-script
 chmod +x /usr/bin/menu
 chmod +x /usr/bin/menun-ssh
 chmod +x /usr/bin/menu_pw_host
