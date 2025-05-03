09c2cbf8dfb6bdb837fd50e2e8dfc1fb9d3104e8
kanghory <132531297+kanghory@users.noreply.github.com>
2025-05-03 19:07:10 +0700

    Update menu-trojan.sh

--- allmenu/menu-trojan.sh
+++ allmenu/menu-trojan.sh
@@ -58,52 +58,23 @@ red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
 
 
-function cektrojan(){
-clear
-echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}            • TROJAN ONLINE NOW •              ${NC} $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+function cektrojanonline(){
+    LOG="/var/log/xray/access.log"
+    USER_LIST=$(grep '^#trojan ' /etc/xray/config.json | cut -d ' ' -f2 | sort | uniq)
 
-for akun in "${data[@]}"
-do
-if [[ -z "$akun" ]]; then
-akun="tidakada"
-fi
+    echo -e "User Trojan yang sedang online dan jumlah IP-nya:\n"
 
-echo -n > /tmp/iptrojan.txt
-data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
-for ip in "${data2[@]}"
-do
-
-jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
-if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >> /tmp/iptrojan.txt
-else
-echo "$ip" >> /tmp/other.txt
-fi
-jum2=$(cat /tmp/iptrojan.txt)
-sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
-done
-
-jum=$(cat /tmp/iptrojan.txt)
-if [[ -z "$jum" ]]; then
-echo > /dev/null
-else
-jum2=$(cat /tmp/iptrojan.txt | nl)
-echo -e "$COLOR1│${NC}   user : $akun";
-echo -e "$COLOR1│${NC}   $jum2";
-fi
-rm -rf /tmp/iptrojan.txt
-done
-
-rm -rf /tmp/other.txt
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
-echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}                • KANGHORY •                 $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+    for user in $USER_LIST; do
+        ip_list=$(grep -w "$user" "$LOG" | awk '{print $3}' | cut -d: -f1 | sort -u)
+        count=$(echo "$ip_list" | wc -l)
+        if [[ $count -gt 0 ]]; then
+            echo "User: $user"
+            echo "  Jumlah IP : $count"
+            echo "  IP Login  :"
+            echo "$ip_list" | sed 's/^/    - /'
+            echo ""
+        fi
+    done
 echo ""
 read -n 1 -s -r -p "   Press any key to back on menu"
 menu-trojan
@@ -346,7 +317,7 @@ case $opt in
 01 | 1) clear ; addtrojan ;;
 02 | 2) clear ; renewtrojan ;;
 03 | 3) clear ; deltrojan ;;
-04 | 4) clear ; cektrojan ;;
+04 | 4) clear ; cektrojanonline ;;
 00 | 0) clear ; menu ;;
 *) clear ; menu-trojan ;;
 esac
