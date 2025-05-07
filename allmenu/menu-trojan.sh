2c3c4ded1f9c7bfe3f68ae1247e5ba388d78cc52
kanghory <132531297+kanghory@users.noreply.github.com>
2025-05-07 19:20:19 +0700

    Update menu-trojan.sh

--- allmenu/menu-trojan.sh
+++ allmenu/menu-trojan.sh
@@ -56,7 +56,7 @@ NC='\e[0m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
-cek_user_trojan_online() {
+xxxcek_user_trojan_online() {
     LOG="/var/log/xray/access.log"
     LIMIT_DIR="/etc/klmpk/limit/trojan/ip"
     TMP="/tmp/cek_trojan_ip.tmp"
