d7782e36e7a241af36276aa7b149671a7119b8f6
kanghory <132531297+kanghory@users.noreply.github.com>
2025-05-12 23:09:47 +0700

    Update add-vless.sh

--- allmenu/add-vless.sh
+++ allmenu/add-vless.sh
@@ -26,6 +26,7 @@ fi
 clear
 
 domain=$(cat /etc/xray/domain)
+mkdir -p /root/akun/vless/
 clear
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
   echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
